dd769f83f0e2c69f4087c209f16ec341014c5927
Richard Perryman <rperrymaniii@gmail.com>
2014-09-22 15:11:46 -0700

    remove bad files

--- local_settings.py
+++
@@ -1,9 +0,0 @@
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'presentation',
-        'USER': 'postgres'
-    }
-}
-
-INTERNAL_IPS = ("127.0.0.1", "10.0.2.2")
